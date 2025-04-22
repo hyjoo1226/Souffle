c774aae9136a0bbda4d64b4e3557d2d030c28ddb
주현호 <sparrow9576@naver.com>
2025-04-22 17:21:22 +0900

    categories, analyses 모듈 생성

--- Back/math-mate/src/app.module.ts
+++ Back/math-mate/src/app.module.ts
@@ -4,8 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { ProblemsModule } from './problems/problems.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AnalysesModule } from './analyses/analyses.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), SubmissionsModule, UsersModule, ProblemsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), SubmissionsModule, UsersModule, ProblemsModule, CategoriesModule, AnalysesModule],
 })
 export class AppModule {}
