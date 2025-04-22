2a9b88794efbdd8b75d5a1a8a7e8b33bfc9e5614
주현호 <sparrow9576@naver.com>
2025-04-22 16:25:59 +0900

    Feat: users, problems 모듈 생성

--- Back/math-mate/src/app.module.ts
+++ Back/math-mate/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SubmissionsModule } from './submissions/submissions.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
+import { ProblemsModule } from './problems/problems.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), SubmissionsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), SubmissionsModule, UsersModule, ProblemsModule],
 })
 export class AppModule {}
