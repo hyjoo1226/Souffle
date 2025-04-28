b0e2a14dd4889c0361bb71b860c28a83e96a342a
seungju <seungju425@gmail.com>
2025-04-28 14:19:46 +0900

    chore: 리액트 프로젝트 생성, 기본 파일 구조 작업, gitignore 작성
    - react+typescript 프로젝트 생성
    - react router dom 라이브러리 설치
    - 프로젝트에 필요한 기본 폴더 생성(gitkeep으로 유지)
    - .gitignore 작성
    - .gitattribute 작성(줄바꿈 에러 해결)

---
+++ frontend/src/routes/Router.tsx
@@ -0,0 +1,26 @@
+// import { Route, Routes } from 'react-router-dom';
+
+// const Router = () => {
+//   return (
+//     <Routes>
+//       {/* 헤더가 없는 페이지 */}
+//       <Route element={<AuthLayout />}>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/" element={<Landing />} />
+//       </Route>
+//       <Route element={<Layout />}>
+//         <Route path="/main" element={<MainPage />} />
+//         <Route path="/koreaAnalysis" element={<KoreaAnalysisPage />} />
+//         <Route element={<PrivateRoute />}>
+//           <Route path="/mypage" element={<MyPage />} />
+//         </Route>
+//         <Route
+//           path="/worldDetail/:country/:category/:period/:keyword/:keyword_mind?"
+//           element={<WorldDetail />}
+//         />
+//       </Route>
+//     </Routes>
+//   );
+// };
+
+// export default Router;
