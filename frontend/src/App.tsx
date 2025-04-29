ebae7d06e90d26fdda1c03fa924e3cb21bac443b
seungju <seungju425@gmail.com>
2025-04-29 14:19:00 +0900

    chore: eslint 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-2xl text-gray-400">세팅완!!!</h1>
+        <h1 className="text-2xl text-gray-700">세팅완!!!</h1>
       </div>
     </>
   );
