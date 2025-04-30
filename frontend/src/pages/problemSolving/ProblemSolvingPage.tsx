be11f75a00a7928fadc9da162b12c48aa1fd428a
seungju <seungju425@gmail.com>
2025-04-30 13:45:54 +0900

    FEAT: solutionarea 컴포넌트 생성

--- frontend/src/pages/problemSolving/ProblemSolvingPage.tsx
+++ frontend/src/pages/problemSolving/ProblemSolvingPage.tsx
@@ -1,5 +1,14 @@
+import SolutionArea from "../../components/problemSolving/SolutionArea";
+
 const ProblemSolvingPage = () => {
-  return <div className="text-gray-700">ProblemSolvingPage</div>;
+  return (
+    <div className="grid grid-cols-12 text-gray-700">
+      <div className="col-span-5 bg-amber-200"></div>
+      <div className="col-span-7 ">
+        <SolutionArea />
+      </div>
+    </div>
+  );
 };
 
 export default ProblemSolvingPage;
