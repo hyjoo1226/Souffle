803dda4a520d8f813d0faa55c1e702255c412b74
이승주 <seungju425@gmail.com>
2025-05-12 10:46:00 +0900

    fix: entrypage 빌드 에러 해결

--- frontend/src/pages/EntryPage.tsx
+++ frontend/src/pages/EntryPage.tsx
@@ -7,20 +7,19 @@ const EntryPage = () => {
   };
   return (
     <div>
-      <div>
-        <nav className="flex gap-4 p-4 bg-gray-300">
-          <Link to="/solving">문제 풀이</Link>
-          <Link to="/analysis/1">풀이 분석</Link>
-          <Link to="/problem-select/">문제 선택</Link>
+      <nav className="flex gap-4 p-4 bg-gray-300">
+        <Link to="/solving">문제 풀이</Link>
+        <Link to="/analysis/1">풀이 분석</Link>
+        <Link to="/problem-select/">문제 선택</Link>
 
-      {/* 임시로 submissionId 1번으로 넣어놨어요 */}
-      <Link to="/review">오답노트</Link>
-      <Link to="/review-list">오답노트 리스트</Link>
-    </nav>
-          {/* 임시로 submissionId 1번으로 넣어놨어요 */}
-          <Link to="/review">오답노트</Link>
-        </nav>
-      </div>
+        {/* 임시로 submissionId 1번으로 넣어놨어요 */}
+        <Link to="/review">오답노트</Link>
+        <Link to="/review-list">오답노트 리스트</Link>
+
+        {/* 임시로 submissionId 1번으로 넣어놨어요 */}
+        <Link to="/review">오답노트</Link>
+      </nav>
+
       <button onClick={googleLogin} className="text-gray-700 display-large">
         구글로그인
       </button>
