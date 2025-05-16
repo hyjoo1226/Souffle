7385b1b3510e10548a10bd46e51343cadf1759ff
starcraftchobo <madeinburundi@gmail.com>
2025-05-16 13:50:14 +0900

    feat: 보고서&학습계획 api 구현 완료

--- data/app/main.py
+++ data/app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 from fastapi import FastAPI, APIRouter, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import ocr_router
+from app.routers import ocr_router, report_router
 from app.core.logging import setup_logging
 from app.core.exceptions import error_to_http_exception
 import logging
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 # API 라우터 설정
 api_router = APIRouter(prefix="/data/api/v1")
 api_router.include_router(ocr_router.router)
-
+api_router.include_router(report_router.router)
 # FastAPI 앱 생성
 app = FastAPI(
     title="수플래 분석 서버",
