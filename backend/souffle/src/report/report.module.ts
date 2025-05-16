7e2077c4c4b542dae19788580d49a6d9c920786f
Ubuntu <ubuntu@ip-172-26-0-98.ap-northeast-2.compute.internal>
2025-05-16 20:33:01 +0900

    refactor: branches merged

--- backend/souffle/src/report/report.module.ts
+++ backend/souffle/src/report/report.module.ts
@@ -4,9 +4,14 @@ import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
 import { UserReport } from 'src/users/entities/user-report.entity';
 import { HttpModule } from '@nestjs/axios';
+import { UserScoreStat } from 'src/users/entities/user-score-stat.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([UserReport])],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([UserReport, User, UserScoreStat]),
+  ],
   controllers: [ReportController],
   providers: [ReportService],
 })
