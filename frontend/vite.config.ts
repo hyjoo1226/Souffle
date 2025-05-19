70b729fe573015a5a02ba1cd7fb00fb3533d9aa6
Ubuntu <ubuntu@ip-172-26-0-98.ap-northeast-2.compute.internal>
2025-05-19 15:06:41 +0900

    fix: vite config ci test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,6 +8,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
+  console.log("BACKEND_URL from env:", env.VITE_APP_API_URL);
   return {
   plugins: [
     react(),
