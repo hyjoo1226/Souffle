d9a30846b36e2aac8101d279aeae2b35e257c67d
Ubuntu <ubuntu@ip-172-26-0-98.ap-northeast-2.compute.internal>
2025-05-19 15:59:30 +0900

    fix: CI env test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,6 +9,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+  return {
   plugins: [
     react(),
     VitePWA({
