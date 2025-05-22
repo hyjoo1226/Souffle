8e2b3f489e924734b405a94272599eddfc645b72
김승우 <te04072@naver.com>
2025-05-22 09:20:06 +0900

    Update .gitignore, .gitlab-ci.yml, builder.sh, docker-compose.ci.yml, docker-compose.backend.yml, docker-compose.data.yml, docker-compose.frontend.yml, docker-compose.yml

--- builder.sh
+++ builder.sh
@@ -1,13 +1,17 @@
 set -e
 
-docker compose build --no-cache frontend
+VITE_APP_API_URL=https://www.souffle.kr/api/v1 docker build --no-cache -t frontend:ci -f frontend/Dockerfile ./frontend
 
-docker create --name tmp-frontend s12p31a304-frontend
+docker create --name tmp-frontend frontend:ci
 
 rm -rf frontend/dist
 
-docker cp tmp-frontend:/app/dist ./frontend/dist
+mkdir -p frontend/dist
+
+docker cp tmp-frontend:/app/dist/. ./frontend/dist/
 
 docker rm tmp-frontend
 
 docker compose up -d --build
+
+docker restart caddy
